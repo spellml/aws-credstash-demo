dddb0a3115163570d2c213cf478129136a3050c4
Aleksey Bilogur <aleksey@residentmar.io>
2020-10-29 12:49:01 -0400

    Add README.

---
+++ README.md
@@ -0,0 +1,3 @@
+# aws-credstash-demo
+
+This repo contains the code associated with the blog post "Fine-grained access controls in Spell using private pip package middleware".
--- scripts/create_example_db.sh
+++ scripts/create_example_db.sh
@@ -33,3 +33,10 @@ credstash --kms-region us-east-1 \
 credstash --kms-region us-east-1 \
     --profile $PROFILE \
     put postgres_db_read_write_user_pwd password
+
+# NOTE: you will not immediately have access to this DB from your local machine or from Spell
+# because you still need to allow in your security group settings. If you are following along
+# with the demo in code, see the section "Troubleshooting connections to your PostgreSQL instance"
+# in https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_ConnectToPostgreSQLInstance.html
+# and the section "Provide access to your DB instance in your VPC by creating a security group"
+# in https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_SettingUp.html.
